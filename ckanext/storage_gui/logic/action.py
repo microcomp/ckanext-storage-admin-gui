d94d4d2d9a15ece3270e8fe2f42e933228a657b7
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-11-26 14:52:00 +0100

    backup

---
+++ ckanext/storage_gui/logic/action.py
@@ -0,0 +1,99 @@
+import logging
+
+from pylons import config
+import sqlalchemy
+import ckan.logic as logic
+import ckan.model as model
+from ckanext.storage_gui.storageGUI import org_free_space_filestore
+from ckanext.storage_gui.model.resource_size import package_get_limit
+
+_or_ = sqlalchemy.or_
+log = logging.getLogger(__name__)
+NotFound = logic.NotFound
+ValidationError = logic.ValidationError
+_get_or_bust = logic.get_or_bust
+
+mb = 1048576
+
+@logic.side_effect_free
+def package_autocomplete(context, data_dict):
+    '''Return a list of datasets (packages) that match a string.
+
+    Datasets with names or titles that contain the query string will be
+    returned.
+
+    :param q: the string to search for
+    :type q: string
+    :param limit: the maximum number of resource formats to return (optional,
+        default: 10)
+    :type limit: int
+
+    :rtype: list of dictionaries
+
+    '''
+    model = context['model']
+    
+    log.info('package_autocomplete call')
+    logic.check_access('package_autocomplete', context, data_dict)
+
+    limit = data_dict.get('limit', 10)
+    q = data_dict['q']
+    owner_org = data_dict.get('owner_org')
+
+    like_q = u"%s%%" % q
+
+    query = model.Session.query(model.PackageRevision)
+    query = query.filter(model.PackageRevision.state == 'active')
+    #query = query.filter(model.PackageRevision.private == False)
+    query = query.filter(model.PackageRevision.current == True)
+    if owner_org:
+        logging.info('got owner org:"%s', owner_org)
+        query = query.filter(model.PackageRevision.owner_org == owner_org)
+    query = query.filter(_or_(model.PackageRevision.name.ilike(like_q),
+                                model.PackageRevision.title.ilike(like_q)))
+    query = query.limit(limit)
+
+    q_lower = q.lower()
+    pkg_list = []
+    for package in query:
+        if package.name.startswith(q_lower):
+            match_field = 'name'
+            match_displayed = package.name
+        else:
+            match_field = 'title'
+            match_displayed = '%s (%s)' % (package.title, package.name)
+        result_dict = {'name':package.name, 'title':package.title,
+                       'match_field':match_field, 'match_displayed':match_displayed}
+        pkg_list.append(result_dict)
+    
+    log.info('custom autocomplete result: %s', pkg_list)
+    resultSet = {'ResultSet': {'Result': pkg_list}}
+    log.info(resultSet)
+    return pkg_list
+
+def organization_available_space(context, data_dict):
+    id = _get_or_bust(data_dict, 'id')
+    group = model.Group.get(id)
+    context["group"] = group
+    if group is None:
+        raise NotFound('Group was not found.')
+    free_space = org_free_space_filestore(id)
+    log.debug('org free space: %s', free_space)
+    if free_space > 0:
+        return True
+    return False
+
+def _get_max_resource_size():
+    return config.get('ckan.max_resource_size', 2)
+
+def package_resource_size_limit(context, data_dict):
+    id = _get_or_bust(data_dict, 'id')
+    pkg = model.Package.get(id)
+    if pkg is None:
+        raise NotFound('Package was not found.')
+    limit = package_get_limit(pkg.id)
+    limit = limit/mb if limit > 0 else _get_max_resource_size()
+    log.info('limit in MiB: %s', limit)
+    return limit
+    
+    
--- ckanext/storage_gui/storageGUI.py
+++ ckanext/storage_gui/storageGUI.py
@@ -1,13 +1,36 @@
 import logging
-import db
+
+from pylons import config
 import ckan.lib.base as base
 import ckan.plugins.toolkit as tk
 import ckan.model as model
-from hurry.filesize import size
 import ckan.logic as logic
-from ckan.common import _, c
+from ckan.common import _, c, request
+import ckan.lib.helpers as h
+
+from ckanext.storage_gui.model import db, resource_size
 
 log = logging.getLogger(__name__)
+NotFound = logic.NotFound
+tb = 1099511627776
+gb = 1073741824
+mb = 1048576
+kb = 1024
+size_endings = [{'text' : 'KiB', 'value' : 'KiB'}, {'text' : 'MiB', 'value' : 'MiB'}, {'text' : 'GiB', 'value' : 'GiB'}, {'text' : 'TiB', 'value' : 'TiB'}]
+
+
+def org_free_space_filestore(org_id):
+    create_storage_stat_table()
+    org_limit = resource_size.organization_get_limit(org_id, 'filestore')
+    if org_limit < 0:
+        org_limit = mb * config.get('ckanext-storage-gui.organization_filestore_limit', 2)
+    data_dict = {'subject_id' : org_id}
+    org_history = db.StorageStat.get(**data_dict)
+    if org_history:
+        org_current_usage = org_history[-1].filestore_usage
+        if org_current_usage < org_limit:
+            return org_limit - org_current_usage
+    return 0
 
 def create_storage_stat_table():
     if db.storage_stat_table is None:
@@ -27,14 +50,218 @@ def insert_storage_stat(subject_id, filestore, database, triplestore):
     session.commit()
 
 class StorageController(base.BaseController):
+    def _check_access(self, user):
+        context = {'user' : user}
+        try:
+            logic.check_access('storage_usage', context)
+        except tk.NotAuthorized, e:
+            log.info(e.extra_msg)
+            tk.abort(401, e.extra_msg)
+
     def _retrieve_data_to_db(self):
         stats = tk.get_action('used_space')()
         stats_org = tk.get_action('used_space_per_org')()
         insert_storage_stat('total', stats['filesystem'], stats['database'], stats['triplestore'])
         for key, org in stats_org.iteritems():
             insert_storage_stat(key, org['filesystem'], org['database'], org['triplestore'])
+
+    def _retrieve_config_attr(self):
+        try:
+            getattr(self, 'organization_filestore_limit')
+        except AttributeError:
+            self.organization_filestore_limit = config.get('ckanext-storage-gui.organization_filestore_limit', 2)
+            self.organization_database_limit = config.get('ckanext-storage-gui.organization_database_limit', 2)
+            self.organization_triplestore_limit = config.get('ckanext-storage-gui.organization_triplestore_limit', 2)
+            self.max_resource_size = config.get('ckan.max_resource_size', 2)
+        
+    def _retrieve_org_storage_limit(self, org_id, storage_type):
+        org_limit = resource_size.organization_get_limit(org_id, storage_type)
+        if org_limit < 0:
+            try:
+                org_limit = mb * getattr(self, 'organization_' + storage_type + '_limit')
+            except AttributeError:
+                org_limit = 0
+        return org_limit       
+    
+    def _get_percentage_usage(self, limit, current):
+        return int(float(current)/limit*100)
+    
+    def _verify_config_storage_option_vs_new(self, storage_type, new_storage_size, new_storage_unit):
+        if new_storage_unit == 'MiB':
+            try:
+                storage_size = getattr(self, 'organization_' + storage_type + '_limit')
+                if storage_size == float(new_storage_size):
+                    return False
+                return True
+            except AttributeError, e:
+                log.exception(e)
+        return False
+    
+    def _convert_to_bytes(self, size, unit):
+        if unit == 'KiB':
+            return size*kb
+        if unit == 'MiB':
+            return size*mb
+        if unit == 'GiB':
+            return size*gb
+        if unit == 'TiB':
+            return size*tb
+    
+    def dataset_storage_edit(self, id, pkg_id):
+        self._check_access(c.user)
+        self._retrieve_config_attr()
+        c.endings = size_endings
+        try:
+            c.group_dict = tk.get_action('organization_show')(data_dict = {'id' : id})
+            if request.method == 'POST':
+                if request.params.get('submit', None):
+                    log.info('POST request params: %s', request.params)
+                    dataset = request.params.get('dataset')
+                    max_resource_size = request.params.get('max_resource_size')
+                    resource_unit = request.params.get('size_unit')
+                    resource_size_bytes = self._convert_to_bytes(float(max_resource_size), resource_unit)
+                    context = {'session' : model.Session, 'user' : c.user}
+                    dataset_id = tk.get_converter('convert_package_name_or_id_to_id')(dataset, context)
+                    log.info('dataset id: %s', dataset_id)
+                    resource_size.package_insert_limit(dataset_id, resource_size_bytes)
+                    h.flash_success(_('Configuration of dataset limit was successfull.'))
+                    return tk.redirect_to('organization_storage', id=id)
+            else:
+                c.dataset = pkg_id if pkg_id else None
+                if pkg_id:
+                    dataset_id = tk.get_converter('convert_package_name_or_id_to_id')(pkg_id, {'session' : model.Session})
+                    size = resource_size.package_get_limit(dataset_id)
+                    if size > 0:
+                        c.size, c.unit = h.localised_filesize(size).split()
+                    else:
+                        c.size = self.max_resource_size 
+                        c.unit = 'MiB'
+                
+        except NotFound:
+            base.abort(404, _('Organization not found'))
             
+        return base.render('organization/storage_dataset_edit.html')
+    
+    def dataset_storage_reset(self, id, pkg_id):
+        self._check_access(c.user)
+        try:
+            context = {'session' : model.Session, 'user' : c.user}
+            dataset_id = tk.get_converter('convert_package_name_or_id_to_id')(pkg_id, context) 
+            resource_size.package_delete_limit(dataset_id)
+            c.group_dict = tk.get_action('organization_show')(data_dict = {'id' : id})
+            h.flash_success(_("Dataset's max. resource size is set to default value."))
+            return tk.redirect_to('organization_storage', id=id)
+        except NotFound:
+            base.abort(404, _('Organization not found'))
+          
+    def org_storage_reset(self, id):
+        self._check_access(c.user)
+        resource_size.organization_reset_limit(id)
+        h.flash_success(_('Configuration reset was successfull.'))
+        return tk.redirect_to('organization_storage', id=id)
+    
+    def org_storage_edit(self, id):
+        self._check_access(c.user)
+        self._retrieve_config_attr()
+        c.endings = size_endings
+        
+        try:
+            c.group_dict = tk.get_action('organization_show')(data_dict = {'id' : id})
+            org_filestore_limit, org_filestore_limit_unit = h.localised_filesize(self._retrieve_org_storage_limit(id, 'filestore')).split()
+            org_db_limit, org_db_limit_unit = h.localised_filesize(self._retrieve_org_storage_limit(id, 'database')).split()
+            org_triplestore_limit, org_triplestore_limit_unit = h.localised_filesize(self._retrieve_org_storage_limit(id, 'triplestore')).split()
+            log.info('request method: %s', request.method)
+            log.info('params: %s', request.params)
+            if request.method == 'POST':
+                if request.params.get('submit', None):
+                    log.info('POST request params: %s', request.params)
+                    database_size = request.params.get('database_size')
+                    database_unit = request.params.get('database_unit')
+                    filestore_size = request.params.get('filestore_size')
+                    filestore_unit = request.params.get('filestore_unit')
+                    triplestore_size = request.params.get('triplestore_size')
+                    triplestore_unit = request.params.get('triplestore_unit')
+                    database_insert = self._verify_config_storage_option_vs_new('database',database_size, database_unit)
+                    filestore_insert = self._verify_config_storage_option_vs_new('filestore',filestore_size, filestore_unit)
+                    triplestore_insert = self._verify_config_storage_option_vs_new('triplestore',triplestore_size, triplestore_unit)
+                    if database_insert:
+                        resource_size.organization_insert_limit(id, 'database', self._convert_to_bytes(float(database_size), database_unit))
+                    else:
+                        resource_size.organization_delete_limit(id, 'database')
+                    if filestore_insert:
+                        resource_size.organization_insert_limit(id, 'filestore', self._convert_to_bytes(float(filestore_size), filestore_unit))
+                    else:
+                        log.debug('deleting organization filestore limit')
+                        resource_size.organization_delete_limit(id, 'filestore')
+                    if triplestore_insert:
+                        resource_size.organization_insert_limit(id, 'triplestore', self._convert_to_bytes(float(triplestore_size), triplestore_unit))
+                    else:
+                        resource_size.organization_delete_limit(id, 'triplestore')
+                    
+                    log.info('before redirect')
+                    h.flash_success(_('Configuration updated.'))
+                    return tk.redirect_to('organization_storage', id=id)
+                elif request.params.get('reset', None):
+                    resource_size.organization_reset_limit(id)
+                    h.flash_success(_('Configuration reset was successfull.'))
+                    return tk.redirect_to('organization_storage', id=id)
+                else:
+                    pass
+                    
+                    
+                
+        except NotFound:
+            base.abort(404, _('Organization not found'))
+        return base.render('organization/storage_edit.html', extra_vars={'filestore_limit': org_filestore_limit,
+                                                                         'filestore_limit_unit': org_filestore_limit_unit,
+                                                                         'database_limit': org_db_limit,
+                                                                         'database_limit_unit': org_db_limit_unit,
+                                                                         'triplestore_limit': org_triplestore_limit,
+                                                                         'triplestore_limit_unit': org_triplestore_limit_unit})
+    
+    def org_storage(self, id):
+        self._check_access(c.user)
+        self._retrieve_config_attr()
+        list_org_history = []
+        create_storage_stat_table()
+        data_dict = {'subject_id' : id}
+        org_history = db.StorageStat.get(**data_dict)
+        for item in org_history:
+            list_org_history.append({'filesystem' : item.filestore_usage,
+                                     'database' : item.database_usage,
+                                     'triplestore' : item.triplestore_usage,
+                                     'time' : item.time.strftime("%H:%M:%S %d.%m.%Y")})
+        
+        org_filestore_limit = self._retrieve_org_storage_limit(id, 'filestore')
+        org_filestore_percentage_usage = self._get_percentage_usage(org_filestore_limit, list_org_history[-1]['filesystem'])
+        org_db_limit = self._retrieve_org_storage_limit(id, 'database')
+        org_db_percentage_usage = self._get_percentage_usage(org_db_limit, list_org_history[-1]['database'])
+        org_triplestore_limit = self._retrieve_org_storage_limit(id, 'triplestore')
+        org_triplestore_percentage_usage = self._get_percentage_usage(org_db_limit, list_org_history[-1]['triplestore'])
+        
+        
+        try:
+            package_list = []
+            c.group_dict = tk.get_action('organization_show')(data_dict = {'id' : id})
+            log.info(c.group_dict)
+            for package in c.group_dict['packages']:
+                result = resource_size.package_get_limit(package['id'])
+                limit = h.localised_filesize(result) if result>0 else self.max_resource_size + ' MiB'
+                package_list.append((package['name'], package['title'], limit))
+            c.package_list = package_list
+        except NotFound:
+            base.abort(404, _('Organization not found'))
+        return base.render('organization/storage.html',extra_vars={'org_history' : list_org_history,
+                                                             'org_filestore_limit' : org_filestore_limit,
+                                                             'org_db_limit' : org_db_limit,
+                                                             'org_triplestore_limit' : org_triplestore_limit,
+                                                             'org_filestore_percentage_usage' : org_filestore_percentage_usage,
+                                                             'org_db_percentage_usage' : org_db_percentage_usage,
+                                                             'org_triplestore_percentage_usage' : org_triplestore_percentage_usage})
+
     def detail(self):
+        self._check_access(c.user)
+        self._retrieve_config_attr()
         context = {'user' : c.user}
         try:
             logic.check_access('storage_usage', context)
@@ -56,19 +283,32 @@ class StorageController(base.BaseController):
         orgs = [(item['display_name'], item['id'])for item in list_of_orgs]
         org_history = db.StorageStat.get(**data_dict)
         for item in org_history:
-            list_org_history.append({'filesystem' : size(item.filestore_usage),
-                                     'database' : size(item.database_usage),
-                                     'triplestore' : size(item.triplestore_usage),
+            list_org_history.append({'filesystem' : item.filestore_usage,
+                                     'database' : item.database_usage,
+                                     'triplestore' : item.triplestore_usage,
                                      'time' : item.time.strftime("%H:%M:%S %d.%m.%Y")})
-            
+        
+        org_filestore_limit = self._retrieve_org_storage_limit(org_id, 'filestore')
+        org_filestore_percentage_usage = self._get_percentage_usage(org_filestore_limit, list_org_history[-1]['filesystem'])
+        org_db_limit = self._retrieve_org_storage_limit(org_id, 'database')
+        org_db_percentage_usage = self._get_percentage_usage(org_db_limit, list_org_history[-1]['database'])
+        org_triplestore_limit = self._retrieve_org_storage_limit(org_id, 'triplestore')
+        org_triplestore_percentage_usage = self._get_percentage_usage(org_db_limit, list_org_history[-1]['triplestore'])
+
         return base.render('storage/detail.html',extra_vars={'org_name' : organization_name,
                                                              'org_history' : list_org_history,
                                                              'org_selected' : org_id,
+                                                             'org_filestore_limit' : org_filestore_limit,
+                                                             'org_db_limit' : org_db_limit,
+                                                             'org_triplestore_limit' : org_triplestore_limit,
+                                                             'org_filestore_percentage_usage' : org_filestore_percentage_usage,
+                                                             'org_db_percentage_usage' : org_db_percentage_usage,
+                                                             'org_triplestore_percentage_usage' : org_triplestore_percentage_usage,
                                                              'orgs' : orgs})
-        
-        
-        
+
     def list(self):
+        self._check_access(c.user)
+        self._retrieve_config_attr()
         context = {'user' : c.user}
         try:
             logic.check_access('storage_usage', context)
@@ -102,9 +342,9 @@ class StorageController(base.BaseController):
         result = db.retrieve_actual_usage(model)
         for res in result:
             if res['subject_id']=='total':
-                total_filestore_usage = size(res['filestore_usage'])
-                total_database_usage = size(res['database_usage'])
-                total_triplestore_usage = size(res['triplestore_usage'])
+                total_filestore_usage = res['filestore_usage']
+                total_database_usage = res['database_usage']
+                total_triplestore_usage = res['triplestore_usage']
                 last_update_time = res['time'].strftime("%H:%M:%S %d.%m.%Y")
             else:
                 org_url = tk.url_for(controller='ckanext.storage_gui.storageGUI:StorageController', action='detail',
@@ -112,11 +352,15 @@ class StorageController(base.BaseController):
                 try:
                     org_info = tk.get_action('organization_show')(data_dict = {'id':res['subject_id'],
                                                                                'include_datasets' : False})
+                    org_limit = resource_size.organization_get_limit(res['subject_id'])
+                    if org_limit < 0:
+                        org_limit = mb * config.get('ckanext-storage-gui.max_organization_size', 2)
                     filtered_content.append({'title' : org_info['display_name'],
                                              'url' : org_url,
-                                             'filesystem' : size(res['filestore_usage']),
-                                             'database' : size(res['database_usage']),
-                                             'triplestore' : size(res['triplestore_usage'])})
+                                             'filesystem' : res['filestore_usage'],
+                                             'database' : res['database_usage'],
+                                             'triplestore' : res['triplestore_usage'],
+                                             'limit' : org_limit})
                 except logic.NotFound as e:
                     log.exception(e)
                 
